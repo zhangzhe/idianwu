de7455941ddea8f3c0f106f3c6a51ea0179fbf37
zhangzhe <gary.zzhang@gmail.com>
2012-08-29 23:00:37 +0800

    first

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Idianwu::Application.config.secret_token = '0a76f98576ab0c0bd127c9d1c5c9913d630b350b2386c89a980f401406e065285ab17992ee0a59aa40ebe0d86211bd9a0d8998168ead8814aa97c497dc3042c1'
